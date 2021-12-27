569db648f5469451b1dc03b26f25b8b5e0cf6c97
garry014 <66904472+garry014@users.noreply.github.com>
2021-12-27 16:13:38 +0800

    init

---
+++ Models/Casket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace afterlife_caretakers.Models
+{
+    public class Casket
+    {
+        [Required]
+        public int id { get; set; }
+        [Required, MinLength(3, ErrorMessage ="Enter at least 3 characters.")]
+        public string name { get; set; }
+        [Required]
+        public string category { get; set; }
+        [Required]
+        public string imageLink { get; set; }
+        [Range(0,19999, ErrorMessage ="Enter valid price from 0 to 19999")]
+        public float price { get; set; }
+        [Required]
+        public int selectedTimes { get; set; }
+        [Required]
+        public Boolean isDeleted { get; set; }
+    }
+}
