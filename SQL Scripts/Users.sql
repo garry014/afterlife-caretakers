c54364fb11f976900c739e5557268aa1222cfb18
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2022-02-10 22:54:20 +0800

    Add files via upload

---
+++ SQL Scripts/Users.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]                INT           IDENTITY (1, 1) NOT NULL,
+    [name]              VARCHAR (500) NOT NULL,
+    [email]             VARCHAR (254) NOT NULL,
+    [phoneno]           VARCHAR (8)   NOT NULL,
+    [password]          VARCHAR (100) NOT NULL,
+    [gender]            VARCHAR (10)  NOT NULL,
+    [usertype]          VARCHAR (100) NOT NULL,
+    [NRIC]              VARCHAR (9)   NOT NULL,
+    [willformID]        VARCHAR (100) NULL,
+    [NRIC_upload]       VARCHAR (100) NULL,
+    [activation_status] VARCHAR (200) NULL,
+    [deathcert_upload]  VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
