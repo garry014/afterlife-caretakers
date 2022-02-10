20a8b2115d3e4cf7a0fd2d2119f88d10e9a58040
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2022-02-10 12:43:47 +0800

    Add files via upload

---
+++ Models/Users.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace afterlife_caretakers.Models
+{
+    public class Users
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required, MaxLength(500)]
+        public string name { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required, MaxLength(8)]
+        public string phoneno { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string gender { get; set; }
+       
+        public string usertype { get; set; }
+        [Required, MaxLength(9)]
+        public string NRIC { get; set; }
+
+        public string willformID { get; set; }
+        
+        public string NRIC_upload { get; set; }
+
+        public string activation_status { get; set; }
+       
+        public string deathcert_upload { get; set; }
+    }
+}
