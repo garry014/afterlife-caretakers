b2311b4fb7766c5a42bf21a889302f91778eb697
kaijieee <76198882+kaijieee@users.noreply.github.com>
2022-02-13 03:40:28 +0800

    Add files via upload

---
+++ SQL Scripts/amdwitness.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[amdwitness] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [name]     NVARCHAR (MAX) NOT NULL,
+    [nric]     NVARCHAR (MAX) NOT NULL,
+    [address]  NVARCHAR (MAX) NOT NULL,
+    [postal]   NVARCHAR (6)   NOT NULL,
+    [homeno]   NVARCHAR (MAX) NOT NULL,
+    [officeno] NVARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
