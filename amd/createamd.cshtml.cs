da0ef85a99e6cc4409b829a17aadfa7f634a1689
kaijieee <76198882+kaijieee@users.noreply.github.com>
2022-02-13 03:32:51 +0800

    Add files via upload

---
+++ amd/createamd.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using afterlife_caretakers.Models;
+using afterlife_caretakers.Services;
+
+namespace afterlife_caretakers.Pages.amd
+{
+    public class createamdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
