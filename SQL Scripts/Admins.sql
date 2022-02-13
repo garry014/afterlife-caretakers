365bba9db4036bb32f2e6edf8d0cf80d14761875
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2022-02-13 08:29:04 +0800

    Create Admins.sql

---
+++ SQL Scripts/Admins.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[Admins] (
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [name]           VARCHAR (500) NOT NULL,
+    [email]          VARCHAR (254) NOT NULL,
+    [password]       VARCHAR (100) NOT NULL,
+    [gender]         VARCHAR (10)  NOT NULL,
+    [admin_role]     NCHAR (20)    NOT NULL,
+    [office_num]     VARCHAR (8)   NOT NULL,
+    [specialisation] VARCHAR (MAX) NULL,
+    [status]         VARCHAR (100) NULL,
+    [clinic_address] VARCHAR (MAX) NULL,
+    [creationuser]   VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
