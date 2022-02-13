beea341e55755d992adb754348ac12af017256af
garry014 <66904472+garry014@users.noreply.github.com>
2022-02-13 10:01:54 +0800

    ui changes

---
+++ Pages/lastmsg/intro.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace afterlife_caretakers.Pages.lastmsg
+{
+    public class introModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
