5225ca7df39db584e8f7f0fdc5779c48d5de3e0a
sekki2323 <73709725+sekki2323@users.noreply.github.com>
2022-02-13 08:29:55 +0800

    Update Users.sql

--- SQL Scripts/Users.sql
+++ SQL Scripts/Users.sql
@@ -11,7 +11,7 @@ CREATE TABLE [dbo].[Users] (
     [NRIC_upload]       VARCHAR (100) NULL,
     [activation_status] VARCHAR (200) NULL,
     [deathcert_upload]  VARCHAR (100) NULL,
-    [deathdate_setting] VARCHAR(50) NULL, 
+    [deathdate_setting] DATETIME NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
