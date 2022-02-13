9d0a0e37bd5ffd4444393eb67769581b10f00b4c
garry014 <66904472+garry014@users.noreply.github.com>
2022-02-13 20:23:38 +0800

    Update View.cshtml.cs

--- Pages/lastmsg/View.cshtml.cs
+++ Pages/lastmsg/View.cshtml.cs
@@ -12,20 +12,27 @@ namespace afterlife_caretakers.Pages.lastmsg
     public class ViewModel : PageModel
     {
         private readonly Services.VideoMemoService _svc;
-        public ViewModel(Services.VideoMemoService service)
+        private readonly Services.BVideoPermissionService _bsvc;
+        public ViewModel(Services.VideoMemoService service, Services.BVideoPermissionService bservice)
         {
             _svc = service;
+            _bsvc = bservice;
         }
         [BindProperty]
         public Video Video { get; set; }
+        [BindProperty]
+        public List<BVideoPermission> AllPermissions { get; set; }
         public IActionResult OnGet()
         {
+            HttpContext.Session.SetInt32("user_id", 1);
             if (HttpContext.Session.GetInt32("user_id") == null)
             {
                 return NotFound();
             }
 
-            Video = _svc.GetVideoByWillMakerId(3); //temp var
+            Video = _svc.GetVideoByWillMakerId((int)HttpContext.Session.GetInt32("user_id"));
+            Console.WriteLine("id: " + (int)HttpContext.Session.GetInt32("user_id"));
+            AllPermissions = _bsvc.GetAllPermissions();
             if (Video == null)
             {
                 return NotFound();
