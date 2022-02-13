fe7ebd97cdec63d34d96edf5774dccb15d40c97d
kaijieee <76198882+kaijieee@users.noreply.github.com>
2022-02-13 21:30:17 +0800

    Update amdinfo.cshtml.cs

--- Pages/amd/amdinfo.cshtml.cs
+++ Pages/amd/amdinfo.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using afterlife_caretakers.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,34 @@ namespace afterlife_caretakers.Pages.amd
 {
     public class amdinfoModel : PageModel
     {
-        public void OnGet()
+        private readonly Services.UserService _svc;
+        [BindProperty]
+        public Users MyUser { get; set; }
+
+        public amdinfoModel(Services.UserService uservice)
         {
+            _svc = uservice;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (HttpContext.Session.GetInt32("user_id") == null)
+            {
+                return NotFound();
+            }
+
+            var current_user = (int)HttpContext.Session.GetInt32("user_id");
+
+
+            MyUser = _svc.GetUserByID(current_user);
+
+            if (MyUser == null)
+            {
+                return NotFound();
+            }
+
+
+            return Page();
         }
     }
 }
