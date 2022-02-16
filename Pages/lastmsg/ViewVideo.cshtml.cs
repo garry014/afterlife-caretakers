d179c428e4e242c8e844e99f346e6c70aa91f721
garry014 <66904472+garry014@users.noreply.github.com>
2022-02-16 19:08:36 +0800

    minor bug fixes

--- Pages/lastmsg/ViewVideo.cshtml.cs
+++ Pages/lastmsg/ViewVideo.cshtml.cs
@@ -32,7 +32,6 @@ namespace afterlife_caretakers.Pages.lastmsg
             
             if (HttpContext.Session.GetString("user_email") == null)
             {
-                Console.WriteLine("1");
                 return NotFound();
             }
             
@@ -41,31 +40,38 @@ namespace afterlife_caretakers.Pages.lastmsg
 
             if (!_svc.PermissionMappingExists(HttpContext.Session.GetString("user_email"), x))
             {
-                Console.WriteLine("2");
                 return NotFound();
             }
             
             Video = _vsvc.GetVideoById(x);
             if (Video == null)
             {
-                Console.WriteLine("3");
                 return NotFound();
             }
 
             User = _usvc.GetUserByID(Video.willMakerID);
             if (User == null)
             {
-                Console.WriteLine("4");
                 return NotFound();
+            }
+
+            DateTime myDate;
+            if (DateTime.TryParse("1/1/2000", out myDate))
+            {
+                // empty
             }
 
             if (User.deathcert_upload == null)
             {
                 ErrorMsg = "You can only view the video after the passing of your loved ones. If your loved ones has already passed on, please (get your executors to) upload a copy of the death cert.";
             }
-            else if (User.deathdate_setting != null && User.deathdate_setting < DateTime.Now.AddDays(Video.releasePeriod * 7))
+            else if (User.deathdate_setting == myDate)
             {
-                ErrorMsg = "Our deepest condolences. We are sorry, your loved ones had only enabled the viewing to you after " + DateTime.Now.AddDays(Video.releasePeriod * 7).ToString() + ". Please come back then.";
+                ErrorMsg = "Our deepest condolences. Our admins are still in the midst of processing the death certificate.";
+            }
+            else if (User.deathdate_setting.AddDays(Video.releasePeriod * 7) > DateTime.Now)
+            {
+                ErrorMsg = "Our deepest condolences. We are sorry, your loved ones has only enabled the viewing to you after " + User.deathdate_setting.AddDays(Video.releasePeriod * 7).ToString() + ". Please come back then.";
             }
 
             return Page();
